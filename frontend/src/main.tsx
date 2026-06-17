3fe57eeec3c3594e10e90e2e0872d6f1688291a6
wangzh <18303706925@163.com>
2026-06-17 19:36:11 +0800

    feat(plugin): add build-time scanner script for plugin components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/router'
+import './plugin-components.generated'
 import './styles/global.less'
 
 const savedTheme = localStorage.getItem('volcano_theme');
